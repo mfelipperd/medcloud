29bce294384b3b99be00e89d0a4ade4308918e2e
mfelipperd <felipperabelodurans@gmail.com>
2022-12-20 12:17:59 -0300

    nada

--- my-app/src/api.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-	// eslint-disable-next-line @typescript-eslint/naming-convention
-	baseURL: 'http://localhost:3333',
-});
-
-export default api;
