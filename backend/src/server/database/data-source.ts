4e4b7d1510f2fa33a41c24cbf1a433d7000e9640
mfelipperd <felipperabelodurans@gmail.com>
2022-12-20 17:54:31 -0300

    rds

--- backend/src/server/database/data-source.ts
+++ backend/src/server/database/data-source.ts
@@ -1,17 +1,18 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
-import { Admin } from '../model/entity/Admins';
 import { Record } from '../model/entity/Records';
 import 'dotenv/config';
 
+const host = 'medcloud.ccax18yz2tko.sa-east-1.rds.amazonaws.com';
+
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    host: process.env.HOST,
+    host: host,//process.env.HOST,
     port: 5441,
-    username: process.env.USERNAME,
-    password: process.env.USERNAME,
-    database: process.env.USERNAME,
+    username: 'medcloud', //process.env.USERNAME,
+    password: 'medcloud', //process.env.USERNAME,
+    database: 'medcloud', //process.env.USERNAME,
     synchronize: true,
     logging: false,
-    entities: [Record, Admin],
+    entities: [Record],
 });
