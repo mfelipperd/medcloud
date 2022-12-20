0bd087976b976748075407be1d65b89f80ff339a
mfelipperd <felipperabelodurans@gmail.com>
2022-12-20 17:25:39 -0300

    var amb, para rds

--- backend/src/server/database/data-source.ts
+++ backend/src/server/database/data-source.ts
@@ -2,14 +2,15 @@ import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import { Admin } from '../model/entity/Admins';
 import { Record } from '../model/entity/Records';
+import 'dotenv/config';
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    host: 'localhost',
+    host: process.env.HOST,
     port: 5441,
-    username: 'test',
-    password: 'test',
-    database: 'test',
+    username: process.env.USERNAME,
+    password: process.env.USERNAME,
+    database: process.env.USERNAME,
     synchronize: true,
     logging: false,
     entities: [Record, Admin],
