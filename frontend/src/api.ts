431e0d3e37d12062d7c4afa738be6a2a7a778ae0
mfelipperd <felipperabelodurans@gmail.com>
2022-12-21 11:38:37 -0300

    Readme

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
 	// eslint-disable-next-line @typescript-eslint/naming-convention
-	baseURL: 'http://localhost:3333',
+	baseURL: 'http://18.228.26.120:3333',
 });
 
 export default api;
